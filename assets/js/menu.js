eca68f17139b59219a85294626a7f1312dfeb1d3
GazuaDev8417 <francadasilvaflamarion@gmail.com>
2025-07-08 19:32:28 -0300

    conclusão de rolagem automática pelos links da barra lateral

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -5,7 +5,7 @@ const searchBtn = document.getElementById('searchBtn')
 const inputContainer = document.getElementById('inputContainer')
 
 
-
+/* MENU LATERAL E OVERLAY */
 openMenu.addEventListener('click', ()=>{
     sidebar.classList.add('active')
     overlay.classList.add('active')
@@ -19,4 +19,15 @@ overlay.addEventListener('click', ()=>{
 
 searchBtn.addEventListener('click', ()=>{
     inputContainer.classList.toggle('active')
+})
+
+/* ROLAGEM DA TELA PELO MENU */
+const navLinks = document.querySelectorAll('aside nav a')
+
+
+navLinks.forEach(link=>{
+    link.addEventListener('click', ()=>{
+        sidebar.classList.remove('active')
+        overlay.classList.remove('active')
+    })
 })
