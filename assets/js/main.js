ba08efbff636e67a5afdde04553231c8b40aa366
GazuaDev8417 <francadasilvaflamarion@gmail.com>
2025-07-09 07:27:00 -0300

    implementação para lógica de dias e horários de funcionamento

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,7 @@ const closePopup = document.getElementById('close-popup')
 
 
 
+
 horariosBtn.addEventListener('click', ()=>{
     overlay.classList.add('active')
     popup.classList.add('active')
@@ -14,4 +15,51 @@ horariosBtn.addEventListener('click', ()=>{
 closePopup.addEventListener('click', ()=>{
     overlay.classList.remove('active')
     popup.classList.remove('active')
+})
+
+
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    const days = ['DOMINGO', 'SEGUNDA', 'TERCA', 'QUARTA', , 'QUINTA', 'SEXTA', 'SÁBADO']
+    const now = new Date()
+    const dayWeek = days[now.getDay()]
+    const currentHour = now.getHours()
+    const currenttMinute = now.getMinutes()
+    const totalMinutes = currentHour * 60 + currenttMinute
+    const horarios = {
+        'SEGUNDA': [1020, 1439],   
+        'TERCA': null,             
+        'QUARTA': [1020, 1439],
+        'QUINTA': [1020, 1439],
+        'SEXTA': null,
+        'SÁBADO': [1020, 1439],
+        'DOMINGO': [1020, 1439],
+    }
+
+
+    document.querySelectorAll('.day-row').forEach(row=>{
+        const alert = document.getElementById('alert')
+        const dayName = row.querySelector('.day-name').textContent.trim().toUpperCase()
+        const time = horarios[dayName]
+
+        if(dayName === dayWeek){
+            if(totalMinutes >= time[0] && totalMinutes <= time[1]){
+                row.style.fontWeight = 'bold'
+                row.style.color = 'green'
+                alert.style.backgroundColor = 'green'
+                alert.textContent = 'ABERTO'
+            }else{
+                row.style.fontWeight = 'bold'
+                row.style.color = 'red'
+                alert.style.display = 'block'
+                alert.textContent = 'AINDA NÃO ESTAMOS FUNCIONANDO'
+            }
+        }
+        
+        if(dayName === dayWeek && !time){
+            row.style.fontWeight = 'bold'
+            row.style.color = 'red'
+            alert.textContent = 'FECHADO HOJE'
+        }
+    })
 })
